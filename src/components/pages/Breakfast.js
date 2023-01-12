f8cea51c8ab136e04508c9064e9a4d0fb20d87a3
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-12 14:34:08 -0600

    update 3

---
+++ src/components/pages/Breakfast.js
@@ -0,0 +1,14 @@
+// Import
+import React from "react";
+
+// Functional Component
+const Breakfast = () => {
+    return(
+        <div>
+            <p>Here is your Breakfast.</p>
+        </div>
+    )
+};
+
+// Export
+export default Breakfast;
---
+++ src/components/pages/Department.js
@@ -0,0 +1,14 @@
+// Import
+import React from "react";
+
+// Functional Component
+const Department = () => {
+    return(
+        <div>
+            <p>Here is your Department.</p>
+        </div>
+    )
+};
+
+// Export
+export default Department;
