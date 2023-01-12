03cae75995d71eb7c4150c70697885574cf324bc
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-12 15:36:48 -0600

    Correctly showing & all sites deployed

--- src/components/utilities/FetchForHomepage.js
+++ src/components/utilities/FetchForHomepage.js
@@ -1,14 +1,18 @@
 // Import
 import React from "react";
+import Navbar from "./Navbar";
+import { Outlet, useNavigate, useOutletContext, useParams} from "react-router-dom"
 
 // Functional Component
 const FetchForHomepage = () => {
-    return(
+    return (
         <div>
-            <p>Here is your Fetch.</p>
+            
+            <Navbar />
+            <Outlet />
         </div>
     )
-};
+}
 
 // Export
 export default FetchForHomepage;
