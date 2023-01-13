4bbbae0d111012eae4cafe3f9cfc2aa2d14334f0
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-13 12:42:12 -0600

    fetching 1

--- src/components/utilities/FetchForHomepage.js
+++ src/components/utilities/FetchForHomepage.js
@@ -1,10 +1,26 @@
 // Import
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Navbar from "./Navbar";
 import { Outlet, useNavigate, useOutletContext, useParams} from "react-router-dom"
 
-// Functional Component
+// State
 const FetchForHomepage = () => {
+    const [department, setDepartment] = useState ([]);
+    const [breakfast, setBreakfast] = useState ([]);
+    const [desserts, setDesserts] = useState ([]);
+    const [entrees, setEntrees] = useState ([]);
+    const [sides, setSides] = useState ([]);
+    const contextObject = {
+        departmentState: [department, setDepartment],
+        breakfastState: [breakfast, setBreakfast],
+        dessertsState: [desserts, setDesserts],
+        entreesState: [entrees, setEntrees],
+        sidesState: [sides, setSides]
+    }
+    const navigate = useNavigate();
+
+
+// Functional Component
     return (
         <div>
             
