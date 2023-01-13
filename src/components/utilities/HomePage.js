8befabf0086025b8192a5674cb192222461360d2
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-13 14:06:53 -0600

    updates

--- src/components/utilities/HomePage.js
+++ src/components/utilities/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     return(
         <div>
             <p id="homepage-title">Dal-Recipe</p>
-            <p id="homepage-subtitle">Where Food Meets Imagination</p>
+            <p id="homepage-subtitle">"Where Food Meets Imagination"</p>
         </div>
     )
 };
