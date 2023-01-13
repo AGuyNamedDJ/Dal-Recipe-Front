0bab574f6bc26878180d38e5980cb27e33ef2309
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-13 14:45:52 -0600

    initial fetch template

---
+++ src/components/utilities/AboutUs.js
@@ -0,0 +1,16 @@
+// Import
+import React from "react";
+
+// Functional Component
+const AboutUs = () => {
+    return(
+        <div>
+            <p>Here is your Template.</p>
+        </div>
+    )
+};
+
+// Export
+export default AboutUs;
+
+// Rememeber IDs(#) are more specific than Classes(.)
--- src/components/utilities/Navbar.js
+++ src/components/utilities/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ({context}) => {
     return (
         <nav class="navbar">
             <Link to="/" id="navbar-text">Home</Link>
-            <Link to="/recipes" id="navbar-text">Recipes</Link>
+            <Link to="/departments" id="navbar-text">Recipes</Link>
             <Link to="/blog" id="navbar-text">Blog</Link>
             <Link to="/aboutus" id="navbar-text">About Us</Link>
             <Link to="/search" id="navbar-text">Search</Link>
