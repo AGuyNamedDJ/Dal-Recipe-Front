40f4eac7798658542d30a77f6744305f3197d9d1
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-16 15:40:39 -0600

    update

--- src/components/pages/Department.js
+++ src/components/pages/Department.js
@@ -17,27 +17,63 @@ const Department = () => {
             <p id="title-text">Recipes & Cooking Ideas</p>
 
             <div class="department-box">
+                {/* Breakfast */}
                 <div id="department-box-breakfast">
                     <img src="https://images.squarespace-cdn.com/content/v1/5d38ca4174e198000123709a/1605034683424-L3T9E7Q22FOIAYA6B19F/image-asset.jpeg?format=1500w" id="breakfast-main-image"></img>
                     {
                         breakfastDepartment.map((department, idx) => {
-                            return <div key={idx}>
-                                <p id="department-box-text">{department.departmentName}</p>
+                            return <div id="department-box-text" key={idx}>
+                                <p>{department.departmentName} </p>
+                                <p id="department-box-subtext">"Delicious and hearty breakfast options to start your day off right."</p>
+                                
                             </div>
                         })
                     }
                 </div>
+
+                {/* Entrees */}
                 <div id="department-box-entrees">
                 <img src="https://assets.vogue.com/photos/62714da82a7f6e1661978205/master/w_2560%2Cc_limit/FN-MetGala-02_00284.jpg" id="entrees-main-image"></img>
                     {
                         entreesDepartment.map((department, idx) => {
-                            return <div key={idx}>
-                                <p id="department-box-text">{department.departmentName}</p>
+                            return <div id="department-box-text" key={idx}>
+                                <p>{department.departmentName}</p>
+                                <p id="department-box-subtext">"Delicious main course options for any meal of the day."</p>
+                            </div>
+                        })
+                    }
+                </div>
+
+                {/* Desserts */}
+                <div id="department-box-desserts">
+                <img src="https://images7.alphacoders.com/409/thumb-1920-409387.jpg" id="desserts-main-image"></img>
+                    {
+                        dessertsDepartment.map((department, idx) => {
+                            return <div id="department-box-text" key={idx}>
+                                <p>{department.departmentName}</p>
+                                <p id="department-box-subtext">"Indulgent sweet treats to satisfy your cravings"</p>
+                            </div>
+                        })
+                    }
+                </div>
+
+                {/* Sides */}
+                <div id="department-box-sides">
+                <img src="https://media.istockphoto.com/id/160117152/photo/korean-food-side-dishes.jpg?s=612x612&w=0&k=20&c=B4W2Ry4FFrN7v6DQ9cHuu-5-6PaTfZpBDkSz24Gxsc0=" id="sides-main-image"></img>
+                    {
+                        sidesDepartment.map((department, idx) => {
+                            return <div id="department-box-text" key={idx}>
+                                <p>{department.departmentName}</p>
+                                <p id="department-box-subtext">"Savor the sides: delicious accompaniments to enhance your meal"</p>
                             </div>
                         })
                     }
                 </div>
             </div>
+
+            <br></br>
+            <br></br>
+            <div id="recipes-explore">Explore Recipes</div>
         </div>
     )
 };
