d5e1113b1a6dfce590bd07ccb9eaf125d24ae25b
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-16 14:00:28 -0600

    progress on rendering state

--- src/components/pages/Department.js
+++ src/components/pages/Department.js
@@ -1,14 +1,34 @@
 // Import
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useOutletContext,useParams } from "react-router-dom";
 
 // Functional Component
 const Department = () => {
+    const { departmentState: [department, setDepartment] } = useOutletContext();
+
     return(
         <div>
-            <p>Here is your Department.</p>
+            <p id="title-text">Recipes & Cooking Ideas</p>
+
+            <div class="department-box">
+                <div id="department-box-breakfast">
+                    {
+                        department.map((department, idx) => {
+                            return <div key={idx}>
+                                <p id="department-box-text">Name of Department: {department.departmentName}</p>
+                            </div>
+                        })
+                    }
+                </div>
+                <div id="department-box-entrees"></div>
+                <div id="department-box-desserts"></div>
+                <div id="department-box-sides"></div>
+            </div>
         </div>
     )
 };
 
 // Export
 export default Department;
+
+// <img src="https://images.squarespace-cdn.com/content/v1/5d38ca4174e198000123709a/1605034683424-L3T9E7Q22FOIAYA6B19F/image-asset.jpeg?format=1500w" id="breakfast-main-image"></img>
--- src/components/utilities/FetchForHomepage.js
+++ src/components/utilities/FetchForHomepage.js
@@ -6,6 +6,7 @@ import { Outlet, useNavigate, useOutletContext, useParams} from "react-router-do
 // State
 const FetchForHomepage = () => {
     const [department, setDepartment] = useState ([]);
+        // The Getter,  The Setter
     const [breakfast, setBreakfast] = useState ([]);
     const [desserts, setDesserts] = useState ([]);
     const [entrees, setEntrees] = useState ([]);
@@ -24,10 +25,19 @@ const FetchForHomepage = () => {
     useEffect(() => {
         async function fetchBreakfastData () {
             try {
-                const response = await fetch ("https://dal-recipe-back.onrender.com/api/breakfast");
+                // Fetch data;
+                const response = await fetch ("https://dal-recipe-back.onrender.com/api/breakfast",{
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                //Translate the promise data;
                 // console.log("This is the Breakfast response:", response)
                 const breakfastData = await response.json();
                 console.log("This is the translated Breakfast JSON response:", breakfastData);
+
+                //Save the data to the State;
+                setBreakfast(breakfastData)
             } catch (error) {
                 console.log("Error fetching Breakfast Data:" )
                 console.log(error);
@@ -40,10 +50,19 @@ const FetchForHomepage = () => {
     useEffect(() => {
         async function fetchDepartmentData () {
             try {
-                const response = await fetch ("https://dal-recipe-back.onrender.com/api/department");
+                // Fetch data;
+                const response = await fetch ("https://dal-recipe-back.onrender.com/api/department",{
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                // Translate the promise data;
                 // console.log("This is the Department response:", response)
                 const departmentData = await response.json();
                 console.log("This is the ranslated Department JSON response:", departmentData);
+
+                // Save the data to the State;
+                setDepartment(departmentData)
             } catch (error) {
                 console.log("Error fetching Department Data:" )
                 console.log(error);
@@ -56,10 +75,19 @@ const FetchForHomepage = () => {
     useEffect(() => {
         async function fetchDessertsData () {
             try {
-                const response = await fetch ("https://dal-recipe-back.onrender.com/api/desserts");
+                // Fetch data
+                const response = await fetch ("https://dal-recipe-back.onrender.com/api/desserts",{
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                // Translate the promise data;
                 // console.log("This is the Desserts response:", response)
                 const dessertsData = await response.json();
                 console.log("This is the ranslated Desserts JSON response:", dessertsData);
+
+                // Save the data to the State
+                setDesserts(dessertsData)
             } catch (error) {
                 console.log("Error fetching Desserts Data:" )
                 console.log(error);
@@ -72,10 +100,19 @@ const FetchForHomepage = () => {
     useEffect(() => {
         async function fetchEntreesData () {
             try {
-                const response = await fetch ("https://dal-recipe-back.onrender.com/api/entrees");
+                // Fetch data;
+                const response = await fetch ("https://dal-recipe-back.onrender.com/api/entrees",{
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                // Translate the promise data;
                 // console.log("This is the Entrees response:", response)
                 const entreesData = await response.json();
                 console.log("This is the ranslated Entrees JSON response:", entreesData);
+
+                //Save the data to the State;
+                setEntrees(entreesData)
             } catch (error) {
                 console.log("Error fetching Entrees Data:" )
                 console.log(error);
@@ -88,10 +125,19 @@ const FetchForHomepage = () => {
     useEffect(() => {
         async function fetchSidesData () {
             try {
-                const response = await fetch ("https://dal-recipe-back.onrender.com/api/sides");
+                // Fetch the data;
+                const response = await fetch ("https://dal-recipe-back.onrender.com/api/sides",{
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                // Translate the promise data;
                 // console.log("This is the Sides response:", response)
                 const sidesData = await response.json();
                 console.log("This is the ranslated Sides JSON response:", sidesData);
+
+                // Save the data to the State;
+                setSides(sidesData)
             } catch (error) {
                 console.log("Error fetching Sides Data:" )
                 console.log(error);
