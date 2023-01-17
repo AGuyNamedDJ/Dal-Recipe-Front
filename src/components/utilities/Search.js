565d9d29445b8f50a49200b393850dad861f149f
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-17 12:15:22 -0600

    almost done

--- src/components/utilities/Search.js
+++ src/components/utilities/Search.js
@@ -1,14 +1,45 @@
 // Import
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
-// Functional Component
 const Search = () => {
-    return(
-        <div>
-            <p>Here is your Template.</p>
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/search/${searchTerm}`);
+    };
+
+    return (
+        <div className="search-container">
+            <h1 className="search-header">Search</h1>
+            <form onSubmit={handleSubmit} className="search-form">
+                <input 
+                    type="text" 
+                    placeholder="Search for a recipe" 
+                    className="search-input"
+                    onChange={handleSearch}
+                    value={searchTerm}
+                />
+                <button className="search-button">Search</button>
+            </form>
+            <ul className="search-results">
+                {searchResults.map((recipe, idx) => {
+                    return (
+                        <li key={idx} className="search-result">
+                            <h2 className="search-result-name">{recipe.name}</h2>
+                            <p className="search-result-type">{recipe.type}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
-    )
+    );
 };
 
-// Export
 export default Search;
--- src/index.js
+++ src/index.js
@@ -81,6 +81,7 @@ const router = createBrowserRouter([
                 path: "/search",
                 element: <Search />
             }
+            
         ]
     }
 ])
