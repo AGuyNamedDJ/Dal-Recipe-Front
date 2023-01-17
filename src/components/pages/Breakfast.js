c9b28d7e1b305f3847d81892567e27fe132badda
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-16 19:49:20 -0600

    satisfied with look

--- src/components/pages/Breakfast.js
+++ src/components/pages/Breakfast.js
@@ -8,22 +8,24 @@ const Breakfast = () => {
 
     {/* This return format is to map all. */}
     return(
-        breakfast && breakfast.length ? breakfast.map((recipe, idx) => {            
+        
+        breakfast && breakfast.length ? breakfast.map((recipe, idx) => {  
+            <p class="title-text">Breakfast</p>          
             return (
-                <div key={idx}>
-                <p id="title-text">Breakfast</p>
-
-                    <div id="recipe-box">
-                        <div id="recipe-box-item">
-
-                            {/* Item Template */}
-                            <p id="department-box-text">{recipe.breakfastName}</p>
-                            <img src={recipe.image} id="recipe-image"></img>
-                            <p>DINNERS</p>
-                            <p id="department-box-text">{recipe.time_to_prepare}</p>
-                            <button id="view-recipe-button"><Link id="view-recipe-button link" to={`/department/breakfast/${recipe.breakfastId}`}>View Recipe</Link></button>
-                        </div>
+                <div key={idx} className="recipe-box">
+                    {/* Image */}
+                    <div>
+                        <img src={recipe.image} id="recipe-image"></img>
                     </div>
+                    
+                    {/* Item Details */}
+                    <div>
+                        <p id="recipe-text">{recipe.breakfastName}</p>
+                        <p id="recipe-type">Serving Size: {recipe.serving_size}</p>
+                        <p id="recipe-ttp">Prep Time: {recipe.time_to_prepare}</p>
+                        <button id="view-recipe-button"><Link id="view-recipe-button link" to={`/department/breakfast/${recipe.breakfastId}`}>View Recipe</Link></button>
+                    </div>
+c
                 </div>
             )
         }) : "No Recipes to display, our Chefs are busy preparing for you!"
